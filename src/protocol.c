134f67e25f1cb02cb0bef69323570041d65de9d4
RitaMartinho <rita.martinho@hotmail.com>
2020-10-18 12:54:22 +0100

    issue 8 almost completed and issue 9 completed yet debian dependent

--- src/protocol.c
+++ src/protocol.c
@@ -1,9 +1,18 @@
 #include "protocol.h"
+#include <ctype.h>
 
-bool
+
+bool 
 isMaster()
 {
-	return false;
+	char *Hostname;   
+	struct hostent *HostInfo;
+
+	Hostname = "google.com";
+	HostInfo = gethostbyname (Hostname);
+
+	if (HostInfo == NULL) return false;
+	else return true;
 }
 
 void
@@ -22,4 +31,84 @@ void
 handlePR(PR_p* message)
 {
 
+}
+unsigned char* ConverMacAddressStringIntoByte(const char *pszMACAddress, unsigned char* pbyAddress)
+{
+    const char cSep = ':';
+	for (int iConunter = 0; iConunter < 6; ++iConunter)
+	{
+		unsigned int iNumber = 0;
+		char ch;
+
+		//Convert letter into lower case.
+		ch = tolower (*pszMACAddress++);
+
+		if ((ch < '0' || ch > '9') && (ch < 'a' || ch > 'f'))
+		{
+			return NULL;
+		}
+
+		//Convert into number. 
+		//       a. If character is digit then ch - '0'
+		//	b. else (ch - 'a' + 10) it is done 
+		//	because addition of 10 takes correct value.
+		iNumber = isdigit (ch) ? (ch - '0') : (ch - 'a' + 10);
+		ch = tolower (*pszMACAddress);
+
+		if ((iConunter < 5 && ch != cSep) || 
+			(iConunter == 5 && ch != '\0' && !isspace (ch)))
+		{
+			++pszMACAddress;
+
+			if ((ch < '0' || ch > '9') && (ch < 'a' || ch > 'f'))
+			{
+				return NULL;
+			}
+
+			iNumber <<= 4;
+			iNumber += isdigit (ch) ? (ch - '0') : (ch - 'a' + 10);
+			ch = *pszMACAddress;
+
+			if (iConunter < 5 && ch != cSep)
+			{
+				return NULL;
+			}
+		}
+		/* Store result.  */
+		pbyAddress[iConunter] = (unsigned char) iNumber;
+		/* Skip cSep.  */
+		++pszMACAddress;
+	}
+	return pbyAddress;
+}
+
+byte*
+getIP()
+{
+    byte* ip = malloc(sizeof(byte)*2);
+    FILE* fp;
+
+    const char mac_add_string [17];
+    unsigned char mac_add_byte[6];
+
+    fp= fopen("/sys/class/net/enp0s5/address","r"); //this only works in debian
+
+    if(fp == NULL)
+    {
+       printf("Could not open mac address file. Errno set to: %d\n", errno);
+       return NULL;
+    }
+    
+    fscanf(fp,"%s",mac_add_string );
+
+    if( ConverMacAddressStringIntoByte(mac_add_string, mac_add_byte)== NULL)
+    {
+        printf("Couldn't convert to mac string to mac bytes\n");
+        return NULL;
+    }
+
+    ip[0]=mac_add_byte[4];
+    ip[1]=mac_add_byte[5];
+
+    return ip;
 }
--- src/protocol.h
+++ src/protocol.h
@@ -1,11 +1,14 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
+#include <netdb.h>
+
 /*
  * Performs a network test
  * Returns true if this node is the master
  * (has an internet connection)
  * false otherwise
+ * not yet completed, lacks "Unset" to update self
  */
 bool
 isMaster();
@@ -19,5 +22,17 @@ handlePB(PB_p* message);
 
 void
 handlePR(PR_p* message);
+
+
+
+/*snippet code provided by anand choubey, consulted on: 
+    https://www.codeproject.com/Articles/35103/Convert-MAC-Address-String-into-Bytes
+* converts mac address string to bytes (does hex to bytes conversion) 
+*/
+unsigned char* 
+ConverMacAddressStringIntoByte(const char *pszMACAddress, unsigned char* pbyAddress);
+
+byte* 
+getIP();
 
 #endif
