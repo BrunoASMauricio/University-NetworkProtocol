f9367541d9aec83b0c718653daf0d3aeebe181fb
Rodrigo Caldas <up201708987@fe.up.pt>
2020-10-27 12:47:15 +0000

    HW_dispatcher - function update

--- src/TX.c
+++ src/TX.c
@@ -20,9 +20,35 @@ sendMessage(void* msg)
 void*
 HW_dispatcher(void*dummy)
 {
-	printf("HW Dispatcher on\n");
-	while(1)
+    printf("HW Dispatcher on\n");
+    
+    int tam_master=0;  
+    byte *b; 
+    
+    socket_s* sockfd = newSocket(PORTHW);
+    startSocket(sockfd);
+
+    while (1)
     {
-        sleep(1);
+        //  Sending========>    ID    sample      //
+       //                       2bytes 2bytes    //               
+        
+        if( Self.IsMaster == 1)
+        {
+            b = (byte*) popFromQueue(&tam_master,Self.InternalQueue);
+            
+            if (b == NULL) tam_master = 0;
+            if (tam_master > 0 )
+            {
+                sendToSocket(sockfd, b ,sizeof(byte)*4);               
+            }
+            tam_master=0; 
+        } 
+        else 
+        {
+            printf(" \n I'm a Slave. Nothing to do here.\n");
+        }
     }
+    free(b);   
+    close(sockfd->port); 
 }
