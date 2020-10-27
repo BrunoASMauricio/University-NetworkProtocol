20c57fbf809fb487e2a325224c2f139659c53374
Rodrigo Caldas <up201708987@fe.up.pt>
2020-10-27 12:48:17 +0000

    Update TX.h

--- src/TX.h
+++ src/TX.h
@@ -1,4 +1,25 @@
 #ifndef TX_H
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include "data.h"
+#include "udp.h"
+#include "protocol.h" 
+#include "main.h"
+
+#define PORTHW     8080 
+#define MAXLINE 1024 
+#define MAXBITSIP 16 //bits 
+#define MAXSAMPLES 10  
+#define MAXBITSSAMPLE 16 // bits
+
+// LEN IN BITS
+#define INITIP  8
+#define LENIP   16
+#define INITSA  48
+#define LENSA   8
+#define INITS   56
+#define LENS    16
+
 #define TX_H
 
 /*
