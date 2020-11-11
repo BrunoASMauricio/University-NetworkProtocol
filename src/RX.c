68f4be4f5e6a049315bfbb3dba25b224d4e91dc6
RitaMartinho <rita.martinho@hotmail.com>
2020-11-11 21:46:12 +0000

    added lastheard stuff

--- src/RX.c
+++ src/RX.c
@@ -81,30 +81,39 @@ void SD_RX(in_message* msg)
 
 void PB_RX(in_message* msg)
 {
-	byte senderIp[2];
+	byte SenderIp[2];
 	byte PBID[2];
-	senderIp[0]=((byte*)msg->buf)[1];
-	senderIp[1]=((byte*)msg->buf)[2];
+	SenderIp[0]=((byte*)msg->buf)[1];
+	SenderIp[1]=((byte*)msg->buf)[2];
 	PBID[0]=((byte *)msg->buf)[3];
 	PBID[1]=((byte *)msg->buf)[4];
 	unsigned short distance	=(((byte *)msg->buf)[5]<< 8) + ((byte *)msg->buf)[6];
+
+
+	timespec Res;
+	unsigned long int Act;
+
 
 	if(Self.Status == Outside)
 	{ //if the node is an outside slave 
 
 		if(distance!= (unsigned short)65535)
 		{
-			NE_TX(senderIp);//sends a NE
+			NE_TX(SenderIp);//sends a NE
 			//startRetransmission(rNE);
 			return; 
 		}	
 	}
 	else
 	{
-		/*if(pibdSearchPair(senderIp,PBID,Self.RoutingPBIDTable)==0)
+		/*if(pibdSearchPair(SenderIp,PBID,Self.RoutingPBIDTable)==0)
 		{
-			pbidInsertPair(senderIP,PBID,Self.RoutingPBIDTable); //stores pair in PBID table
-			routInsertOrUpdateEntry(Self.Table, senderIp, distance, 0,0); //stores distance when receiveing PB so later when it receives PC can update
+			pbidInsertPair(SenderIP,PBID,Self.RoutingPBIDTable); //stores pair in PBID table
+
+			clock_gettime(CLOCK_REALTIME, &Res);
+			Act = Res.tv_sec * (int64_t)1000000000UL + Res.tv_nsec;
+
+			routInsertOrUpdateEntry(Self.Table, SenderIp, distance, 0,0,Act); //stores distance when receiveing PB so later when it receives PC can update
 			PR_TX(senderIp, PBID, msg->SNR);
 			startRetransmission(rPR);
 		}*/
@@ -117,25 +126,29 @@ void PB_RX(in_message* msg)
 void PR_RX(in_message* msg)
 {
 	byte SenderIp[2];
-	byte originatorIp[2];
+	byte OriginatorIp[2];
 	byte PBID[2];
 	byte SNRofSentPB;
 	SenderIp[0]=((byte*)msg->buf)[1];
 	SenderIp[1]=((byte*)msg->buf)[2];
-	originatorIp[0]=((byte*)msg->buf)[3];
-	originatorIp[1]=((byte*)msg->buf)[4];
+	OriginatorIp[0]=((byte*)msg->buf)[3];
+	OriginatorIp[1]=((byte*)msg->buf)[4];
 	PBID[0]=((byte *)msg->buf)[5];
 	PBID[1]=((byte *)msg->buf)[6];
 	unsigned short distance =(((byte *)msg->buf)[7]<<8) + ((byte *)msg->buf)[8];
 	SNRofSentPB=((byte *)msg->buf)[9];
 	
+	timespec Res;
+	unsigned long int Act;
 
-	if(originatorIp[0]== Self.IP[0] && originatorIp[1]== Self.IP[1]) //the node is receiving a PR from a PB it generated
+	if(OriginatorIp[0]== Self.IP[0] && OriginatorIp[1]== Self.IP[1]) //the node is receiving a PR from a PB it generated
 	{
 
+		clock_gettime(CLOCK_REALTIME, &Res);
+		Act = Res.tv_sec * (int64_t)1000000000UL + Res.tv_nsec;
 		//somehow update in routTable using existance distance, SNRofSentPB (remote snr) and msg->snr (local snr)
 		//distance=updateDistance(distance, SNRofSentPB, msg->snr) or something like that 
-		routInsertOrUpdateEntry(Self.Table, SenderIp, distance, 0, 0);
+		//routInsertOrUpdateEntry(Self.Table, SenderIp, distance, 0, 0,Act);
 		PC_TX(SenderIp,PBID,msg->SNR);
 		Self.RoutingPBID++;
 	}
