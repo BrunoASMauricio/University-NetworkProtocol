c93846d67d73815d8332fce75fef2faffbe25532
AntonioMendes <up201608357@fe.up.pt>
2020-11-12 23:29:13 +0000

    a few changes and corrections

--- src/RX.c
+++ src/RX.c
@@ -28,10 +28,13 @@ WS_listener(void* dummy)
 void SD_RX(in_message* msg)
 {
 	int math = 15; //0000 1111
+
+	int SampleNum=0;
+		SampleNum = ((byte*)msg->buf)[6];
 	
 	if(((byte*)msg->buf)[0]&math != 1)
 	{
-	fatalErr("Error: how did you even get here, a not SD packet is inside SD, message[0]) %d", ((byte*)msg->buf)[0]&math;
+	fatalErr("Error: how did you even get here, a not SD packet is inside SD, message[0]) %d", ((byte*)msg->buf)[0]&math);
 	}
 
 	if(((byte*)msg->buf)[2] == 0)
@@ -39,19 +42,38 @@ void SD_RX(in_message* msg)
 		fatalErr("Error: TTL was 0 when should not");
 	}
 
+	//caso seja master vai para a Q do HW
 	if(Self.IsMaster == true)
 	{
 		//add para a queue da interface de HW do rodrigo [IP | SAMPLE]  e depois mudar para isto[IP | SAMPE | TIMESTAMP]
 		//ainda nao me disseram onde vinha o a timestamp por isso vai ficar aqui parado á espera.
-		byte DataToHW[((byte*)msg->buf)[7] + 2];
+
+		byte DataToHW[((byte*)msg->buf)[6] + 2];
 		DataToHW[0] = ((byte*)msg->buf)[1];
 		DataToHW[1]=((byte*)msg->buf)[4];//dao source IP á HW
+
+		int SizeOfPacket;
+		SizeOfPacket = getPacketSize(DataToHW);
+
+		addToQueue( newInMessage(SizeOfPacket + SampleNum , DataToHW ,res), SizeOfPacket , Self.OutboundQueue, 1);
 
 		for  (int i = 0; i < ((byte*)msg->buf)[7]; i++)
 		{
 			DataToHW[i+2] = ((byte*)msg->buf)[7+i];
 		}
-		
+	
+
+	}
+
+	//Caso seja slave faz foward para o proximo node passando as ser proxy
+	if(Self.IsMaster == false)
+	{
+		/*Add the Source IP to the Sub-Slaves, updating LastHeard
+		(Note @AntonioMendes think this is already done in SD_TX if it enters there not sure)*/
+
+		//sends the sample nº to SD_TX 
+		SD_TX(SampleNum);
+
 
 	}
 }
