f223be05afaa37583999a53d5410dc687475ac7d
BrunoASMauricio <brunoasmauricio@gmail.com>
2020-12-30 13:41:47 +0000

    Small changes (removed forgotten %d and unnecessary variable)

--- src/RX/WF.c
+++ src/RX/WF.c
@@ -6,7 +6,6 @@ WF_listener()
 	char buff[MAXIMUM_PACKET_SIZE*2];		// *2 to prevent overflow on buff+PrevBytes
 	int PacketSize;
 	int ReadBytes = 0;
-	int PrevBytes = 0;
 	unsigned int received_messages = 0;
 	in_message message;
 	timespec res;
@@ -26,7 +25,6 @@ WF_listener()
 		perror("Setsockopt");
 		exit(1);
 	}
-	PrevBytes = 0;
 	while(1)
 	{
 		while((ReadBytes = getFromSocket(Meta.WF_RX, buff)) == -1)
@@ -35,16 +33,14 @@ WF_listener()
 		}
 
 		clock_gettime(CLOCK_REALTIME, &res);
-		printf("\t\t-------Node got message (%d/%d bytes) total of %d!!-------\n", ReadBytes, ++received_messages);
+		printf("\t\t-------Node got message (%d bytes) total of %d!!-------\n", ReadBytes, ++received_messages);
 
 		newInMessage(&message, ReadBytes, buff, res);
 
-		while((ReadBytes = getFromSocket(Meta.WF_RX, &(message.PBE))) == -1)
+		while(getFromSocket(Meta.WF_RX, &(message.PBE)) == -1)
 		{
 			continue;
 		}
-
-		assert(ReadBytes == sizeof(message.PBE));
 
 		printf("Received full correct message! Received SNR: %u\n", message.PBE);
 		printMessage(buff, ReadBytes);
