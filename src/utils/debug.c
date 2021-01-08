f782b5465b7467ca2a7a2bc65ad8862bb34b8cf2
BrunoASMauricio <brunoasmauricio@gmail.com>
2021-01-08 19:15:24 +0000

    Added time to output

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -102,9 +102,26 @@ printfLog(const char *fmt, ...)
 	va_list args;
 	if(!Meta.Quiet)
 	{
+		int hours, minutes, seconds, day, month, year;
+
+		// time_t is arithmetic time type
+		time_t now;
+
+		// Obtain current time
+		// time() returns the current time of the system as a time_t value
+		time(&now);
+		struct tm *local = localtime(&now);
+		hours = local->tm_hour;          // get hours since midnight (0-23)
+		minutes = local->tm_min;         // get minutes passed after the hour (0-59)
+		seconds = local->tm_sec;         // get seconds passed after minute (0-59)
+
+		day = local->tm_mday;            // get day of month (1 to 31)
+		month = local->tm_mon + 1;       // get month of year (0 to 11)
+		year = local->tm_year + 1900;    // get year since 1900
+
 		va_start(args, fmt);
 		pthread_mutex_lock(&(Self.OutputLock));
-		fprintf(stdout, "[%c] [!]", getThreadChar());
+		fprintf(stdout, "[%c] [!] [%02d:%02d:%02d]", getThreadChar(), hours, minutes, seconds);
 		vfprintf(stdout, fmt, args);
 		pthread_mutex_unlock(&(Self.OutputLock));
 		va_end(args);
